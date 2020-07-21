1cdbfd94af4691a21eaa9566ccc5f784d8cbbb2c
Rahul S <sudhanaboina.rahul@gmail.com>
2020-07-21 08:24:21 +0530

    Added comments

--- routers.js
+++ routers.js
@@ -4,6 +4,7 @@ import randomstring from 'randomstring'; //generating a random string
 import sqlite3 from  'sqlite3'; 
 const router = express.Router();
 
+//This function to used to set the protocol of URL, if the given URL doesn't has a protocol
 function setHttp(link){
   if (link.search(/^http[s]?\:\/\//) == -1) {
       link = 'http://' + link;
@@ -20,24 +21,27 @@ let db=new sqlite3.Database('./urls.db',(err) => {
 });
 
 router.get('/', (req, res) => {
-    //Redirect to the app if user tries to reach the endpoint
+    //Redirect to the app if user tries to open the endpoint
     return res.status(301).redirect('https://rahuls.github.io/app');
 });
 
 
 router.get('/:surl', (req, res) => {
-    //Get the long URL for the short URL
+    //This handles the request for the original URL
+
+    //Get the path and remove the '/'
     let path=req.originalUrl;
     path=path.slice(1);
     
-    let sql = 'select lurl from urls where surl = ?';
+    //Check in urls table if there is a URL associated with path
+    let sql = 'SELECT lurl FROM urls WHERE surl = ?';
     db.get(sql,[path],(err,row) => {
       if(err){
         return console.error(err.message);
       }
-      if(row){
+      if(row){ //If there is a URL, then redirect to it.
         return res.status(301).redirect(setHttp(row.lurl));
-      }else{
+      }else{ //Show error if there is no URL.
         return res.status(400).json("The short url doesn't exists in our system.");
       }
     });
@@ -45,30 +49,32 @@ router.get('/:surl', (req, res) => {
 
 
 router.post('/', (req, res) => {
-    console.log(req.body);
+    //Get the long URL entered by user
     let lurl = req.body.lurl;
     let surl='';
-    let sql = 'select surl from urls where lurl = ?';
+
+    //Check if the lurl already exists in DB. If yes return the surl found in db
+    //if not create a new one
+    let sql = 'SELECT surl FROM urls WHERE lurl = ?';
     db.get(sql,[lurl],(err,row) => {
       if(err){
         console.log(err.message);
       }
-      if(row){
-          return res.json({surl: row.surl});
-      }else{
-            let key = randomstring.generate({
+      if(row){ 
+          return res.json({"surl": row.surl});
+      }else{//create a new short url.
+            surl = randomstring.generate({
             length: 6,
             charset: 'alphabetic'
-              });
-        
-            sql = "INSERT INTO urls(surl,lurl) VALUES('"+key+"','"+lurl+"')";
-            surl=key;
-            console.log(surl);
+            });
             
+            
+            sql = `INSERT INTO urls(surl,lurl) VALUES('${surl},${lurl}');`
             db.run(sql)
-            console.log('Insert succesful');
+
+            console.log(`${surl} Insert succesful`);
             
-            return res.json({surl: surl});
+            return res.json({"surl": surl});
       }
     });
 });
